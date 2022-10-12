cc72bb99be4084012e0e28f83e3e46bae0d2f6fa
Amgdboi <mattblicharz8@gmail.com>
2022-10-12 17:27:14 +0100

    Initial commit

---
+++ html/main.js
@@ -0,0 +1,12 @@
+//Date variables
+let data = new Date().getDate()
+let month = new Date().getMonth()
+let neineleven = new Date("September 11, 2023 00:00:00").getTime
+
+
+// if statements
+if(data === 11){
+if(month === 8){
+document.getElementById("test").innerHTML = "HAPPY 9/11"
+ } 
+}
